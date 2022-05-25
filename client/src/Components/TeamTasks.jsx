009eca0f30dd6b4ea1a2d1227aa16e9cdeaf62d0
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-05-25 16:32:53 +0800

    Add (dummy) buttons for editing and deleting tasks

--- client/src/Components/TeamTasks.jsx
+++ client/src/Components/TeamTasks.jsx
@@ -11,13 +11,17 @@ import { TableContainer } from "@mui/material";
 import { Box } from "@mui/material";
 import React from "react";
 import AddIcon from '@mui/icons-material/Add';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import { useState } from "react";
 
 function getTeamMemberNames() {
     // For now, we assume that all teams have five members.
     return ["Team Member 1 (You)", "Team Member 2", "Team Member 3", "Team Member 4", "Team Member 5"];
 }
 
-function getTasks() {
+function getTasksFromAPI() {
+    // Pretend data from a pretend backend (to be implemented).
     return [{name: "Lecture 1", week: "1", doneByMember1: true, doneByMember2: true, doneByMember3: false, doneByMember4: true, doneByMember5: true},
             {name: "Lecture 2", week: "2", doneByMember1: false, doneByMember2: true, doneByMember3: false, doneByMember4: false, doneByMember5: false},
             {name: "Lecture 3", week: "3", doneByMember1: true, doneByMember2: false, doneByMember3: false, doneByMember4: false, doneByMember5: false}];
@@ -34,11 +38,15 @@ function displayTaskAsRow(task) {
             <TableCell align="center"> <Checkbox defaultChecked={task.doneByMember3} disabled /> </TableCell>
             <TableCell align="center"> <Checkbox defaultChecked={task.doneByMember4} disabled /> </TableCell>
             <TableCell align="center"> <Checkbox defaultChecked={task.doneByMember5} disabled /> </TableCell>
+            <TableCell align="center"> <EditRoundedIcon onClick={() => alert("We have not yet implemented editing the name of tasks.")}/> </TableCell>
+            <TableCell align="center"> <DeleteRoundedIcon onClick={() => alert("We have not yet implemented deleting tasks.")}/> </TableCell>
         </TableRow>
     )
 }
 
 function TeamTasks() {
+    const [teamTasks, setTeamTasks] = useState(getTasksFromAPI());
+
     return (
         <Box>
             <Typography variant="h4" gutterBottom>
@@ -55,10 +63,12 @@ function TeamTasks() {
                         <TableCell align="center"> {getTeamMemberNames()[2]} </TableCell>
                         <TableCell align="center"> {getTeamMemberNames()[3]} </TableCell>
                         <TableCell align="center"> {getTeamMemberNames()[4]} </TableCell>
+                        <TableCell> </TableCell>
+                        <TableCell> </TableCell>
                     </TableHead>
 
                     <TableBody>
-                        {getTasks().map(task => displayTaskAsRow(task))}
+                        {teamTasks.map(task => displayTaskAsRow(task))}
                     </TableBody>
 
                 </Table>
@@ -68,6 +78,7 @@ function TeamTasks() {
                 color="primary"
                 aria-label="add"
                 sx={{ marginTop: "30px" }}
+                onClick={() => alert("We have not yet implemented adding tasks.")}
             >
                 <AddIcon />
 
