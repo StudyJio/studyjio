7047e0188e56fbb0e9906e67f0849a0408c94166
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-06-04 13:44:03 +0800

    Refactor supabase.js and .env.local
    Last time, the Supabase URL and key were hardcoded in supabase.js. Now, they are read from .env.local.

--- client/src/supabase.js
+++ client/src/supabase.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://raqxffxcknwwrzqjpemc.supabase.co";
-const supabaseAnonKey =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhcXhmZnhja253d3J6cWpwZW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM0Njc4OTMsImV4cCI6MTk2OTA0Mzg5M30.QcWHOSFUiR8oem3p-ghYQ9RxRVyoIS_e6O6wrkarGL8";
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_KEY,
+);
