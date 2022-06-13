d0297cc375e8ce79d07bd053a11ea3ae689b5427
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2022-06-13 17:01:35 +0800

    Create location selector in TeamPreferences.jsx

--- client/src/Components/TeamPreferences.jsx
+++ client/src/Components/TeamPreferences.jsx
@@ -8,12 +8,16 @@ import { FormControlLabel } from "@mui/material";
 import { Radio } from "@mui/material";
 import { Button } from "@mui/material";
 import { Step, StepLabel, Stepper } from "@mui/material";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { TextField } from "@mui/material";
 import { Link } from "@mui/material";
 import { Grid } from "@mui/material";
 import { Slider } from "@mui/material";
 import { withStyles } from "@mui/material";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import { ListSubheader } from "@mui/material";
 
 const MyModules = (
     <>
@@ -37,7 +41,7 @@ function StudyingPersonalityColumnGenerator(leftLabel, rightLabel) {
         { value: 11, label: rightLabel },
     ];
 
-    // sx={{mx: 2}} behaves goofily within a Slider, so I had to uglyly wrap it in a Box.
+    // sx={{mx: 4, my: 2}} behaves goofily within a Slider, so I had to uglily wrap it in a Box.
 
     return (
         <Box sx={{mx: 4, my: 2}}>
@@ -83,19 +87,62 @@ const MyStudyingPersonality = (
     </>
 );
 
-const MyLocation = (
-    <>
-        <Typography variant="h5">
-            My Location
-        </Typography>
+const hallsOfResidence = ["Eusoff Hall", "Kent Ridge Hall", "King Edward VII Hall", "Raffles Hall", "Sheares Hall", "Temasek Hall"];
+const residentialColleges = ["Ridge View Residential College", "College of Alice & Peter Tan", "NUS College", "Residential College 4", "Tembusu College"];
+const studentResidences = ["UTown Residence", "Prince George's Park Residence"];
 
-        <Typography>
-            Please enter your location...
-        </Typography>
-    </>
-);
+function stringToMenuItem(string) {
+    return (<MenuItem value={string}>{string}</MenuItem>);
+}
 
-const pages = [MyModules, MyStudyingPersonality, MyLocation];
+const mrtStations = ["Admiralty", "Aljunied", "Ang Mo Kio", "Aviation Park", "Bahar Junction", "Bartley", "Bayfront", "Bayshore", "Beauty World", "Bedok", "Bedok North", "Bedok Reservoir", "Bedok South", "Bencoolen", "Bendemeer", "Bishan", "Boon Keng", "Boon Lay", "Botanic Gardens", "Braddell", "Bras Basah", "Brickland", "Bright Hill", "Buangkok", "Bugis", "Bukit Batok", "Bukit Batok West", "Bukit Brown", "Bukit Gombak", "Bukit Panjang", "Buona Vista", "Caldecott", "Canberra", "Cantonment", "Cashew", "Changi Airport", "Changi Airport Terminal 5", "Chinatown", "Chinese Garden", "Choa Chu Kang", "Choa Chu Kang West", "City Hall", "Clarke Quay", "Clementi", "Commonwealth", "Corporation", "Dakota", "Defu", "Dhoby Ghaut", "Dover", "Downtown", "Elias", "Enterprise", "Esplanade", "Eunos", "Expo", "Farrer Park", "Farrer Road", "Fort Canning", "Founders' Memorial", "Gardens by the Bay", "Gek Poh", "Geylang Bahru", "Great World", "Gul Circle", "HarbourFront", "Havelock", "Haw Par Villa", "Hillview", "Holland Village", "Hong Kah", "Hougang", "Hume", "Jalan Besar", "Joo Koon", "Jurong East", "Jurong Hill", "Jurong Pier", "Jurong Town Hall", "Jurong West", "Kaki Bukit", "Kallang", "Katong Park", "Kembangan", "Kent Ridge", "Keppel", "Khatib", "King Albert Park", "Kovan", "Kranji", "Labrador Park", "Lakeside", "Lavender", "Lentor", "Little India", "Lorong Chuan", "Loyang", "MacPherson", "Marina Bay", "Marina South", "Marina South Pier", "Marine Parade", "Marine Terrace", "Marsiling", "Marymount", "Mattar", "Maxwell", "Mayflower", "Mount Pleasant", "Mountbatten", "Nanyang Crescent", "Nanyang Gateway", "Napier", "Newton", "Nicoll Highway", "Novena", "one-north", "Orchard", "Orchard Boulevard", "Outram Park", "Pandan Reservoir", "Pasir Panjang", "Pasir Ris", "Pasir Ris East", "Paya Lebar", "Peng Kang Hill", "Pioneer", "Potong Pasir", "Prince Edward Road", "Promenade", "Punggol", "Punggol Coast", "Queenstown", "Raffles Place", "Redhill", "Riviera", "Rochor", "Sembawang", "Sengkang", "Serangoon", "Serangoon North", "Shenton Way", "Siglap", "Simei", "Sixth Avenue", "Somerset", "Springleaf", "Stadium", "Stevens", "Sungei Bedok", "Sungei Kadut", "Tai Seng", "Tampines", "Tampines East", "Tampines North", "Tampines West", "Tan Kah Kee", "Tanah Merah", "Tanjong Katong", "Tanjong Pagar", "Tanjong Rhu", "Tavistock", "Tawas", "Teck Ghee", "Telok Ayer", "Telok Blangah", "Tengah", "Tengah Park", "Tengah Plantation", "Tiong Bahru", "Toa Payoh", "Toh Guan", "Tuas Crescent", "Tuas Link", "Tuas West Road", "Tukang", "Ubi", "Upper Changi", "Upper Thomson", "West Coast Extension", "Woodlands", "Woodlands North", "Woodlands South", "Woodleigh", "Xilin", "Yew Tee", "Yio Chu Kang", "Yishun"];
+
+const CampusLocationMenuItems = [
+    <ListSubheader>Halls of Residence</ListSubheader>,
+    hallsOfResidence.map(stringToMenuItem),
+    <ListSubheader>Residential Colleges</ListSubheader>,
+    residentialColleges.map(stringToMenuItem),
+    <ListSubheader>Student Residences</ListSubheader>,
+    studentResidences.map(stringToMenuItem)
+];
+
+const MRTStationMenuItems = mrtStations.map(stringToMenuItem);
+
+function MyLocation(onOrOffCampus, setOnOrOffCampus, locationSelected, setLocationSelected) {
+    return (
+        <>
+            <Typography variant="h5" sx={{ mb: 2 }}>
+                My Location
+            </Typography>
+
+            <FormControl sx={{ display: 'block' }}>
+                <FormLabel id="on-campus-or-off-campus">I stay...</FormLabel>
+                <RadioGroup
+                    defaultValue="on campus"
+                    value={onOrOffCampus}
+                    name="radio-buttons-group"
+                    onChange={(event) => {setOnOrOffCampus(event.target.value); setLocationSelected(null)}}
+                >
+                    <FormControlLabel value="on campus" control={<Radio />} label="...on campus..." />
+                    <FormControlLabel value="off campus" control={<Radio />} label="...off campus..." />
+                </RadioGroup>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel id="location-selection">
+                    {onOrOffCampus === "on campus" ? "...at..." : "...nearest to..."}
+                </FormLabel>
+                <Select
+                    value={locationSelected}
+                    onChange={(event) => {setLocationSelected(event.target.value)}}
+                >
+                    {onOrOffCampus === "on campus" ? CampusLocationMenuItems : MRTStationMenuItems}
+                </Select>
+            </FormControl>
+        </>
+    );
+}
+
 
 function TeamPreferences() {
 
@@ -109,6 +156,11 @@ function TeamPreferences() {
     function handleClickBackButton() {
         setActiveStep(activeStep - 1);
     }
+
+    const [onOrOffCampus, setOnOrOffCampus] = useState("on campus"); // Either "on campus" or "off campus".
+    const [locationSelected, setLocationSelected] = useState(); // Either an MRT station or a hostel.
+
+    const pages = [MyModules, MyStudyingPersonality, MyLocation(onOrOffCampus, setOnOrOffCampus, locationSelected, setLocationSelected)];
 
     function handleClickNextSaveButton() {
         if (activeStep === 2) {
@@ -151,13 +203,13 @@ function TeamPreferences() {
             </Paper>
 
             {/* A button that is hidden for the first step, and displays "BACK" for all other steps. */}
-            <Button
+            {(activeStep !== 0) && <Button
                 variant="outlined"
                 sx={{ mt: 2 }}
                 onClick={handleClickBackButton}
             >
-                HIDDEN/BACK
-            </Button>
+                BACK
+            </Button>}
 
             {/* A button that displays "NEXT" for the first two steps, and "SAVE" for the last step. */}
             <Button
@@ -165,7 +217,7 @@ function TeamPreferences() {
                 sx={{ mt: 2, ml: 2 }}
                 onClick={handleClickNextSaveButton}
             >
-                NEXT/SAVE
+                {(activeStep === 2) ? "SAVE" : "NEXT"}
             </Button>
 
         </>
