7fe78882b812771eceef42e384e12c17888c49de
lyihan00 <100853376+lyihan00@users.noreply.github.com>
2022-06-24 15:38:41 +0800

    Update MyAccount.jsx
    Reset password not working.

--- client/src/Components/MyAccount.jsx
+++ client/src/Components/MyAccount.jsx
@@ -5,188 +5,256 @@ import { Input } from "@mui/material";
 import { Popover } from "@mui/material";
 import { Paper } from "@mui/material";
 import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { useState } from "react";
-import UploadFileRoundedIcon from '@mui/icons-material/UploadFileRounded';
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import { Box, textAlign } from "@mui/system";
+import { useState, useRef } from "react";
+import UploadFileRoundedIcon from "@mui/icons-material/UploadFileRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import { supabase } from "../supabase";
+import bcrypt from "bcryptjs";
 
 export default function MyAccount() {
+  /**
+   * Variables and functions used by the popover menu which opens when the profile picture is clicked.
+   */
 
-    /**
-     * Variables and functions used by the popover menu which opens when the profile picture is clicked.
-     */
+  // The element (El) to which the popover menu is anchored. When the element is null, the popover menu is not displayed.
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    // The element (El) to which the popover menu is anchored. When the element is null, the popover menu is not displayed.
-    const [anchorEl, setAnchorEl] = useState(null);
+  const isProfilePictureMenuOpen = Boolean(anchorEl);
 
-    const isProfilePictureMenuOpen = Boolean(anchorEl);
+  const profilePictureMenuId = isProfilePictureMenuOpen
+    ? "profile-picture-menu"
+    : undefined;
 
-    const profilePictureMenuId = isProfilePictureMenuOpen ? 'profile-picture-menu' : undefined;
+  const user = supabase.auth.user();
+  const usernameInputRef = useRef();
+  // const emailInputRef = useRef();
+  // const oldPasswordInputRef = useRef();
+  // const newPasswordInputRef = useRef();
+  // const confirmNewPasswordInputRef = useRef();
 
-    // When the profile picture is clicked, ...
-    function handleClickProfilePicture(event) {
-        setAnchorEl(event.currentTarget);
-    }
+  // Save username changes to Supabase upon clicking "Save Changes"
+  const saveUsernameHandler = async (event) => {
+    event.preventDefault();
 
-    function handleCloseProfilePictureMenu() {
-        setAnchorEl(null);
-    }
+    const enteredUsername = usernameInputRef.current.value;
 
-    function handleClickRemovePicture() {
-        // TODO: Delete the user's current profile picture from the database.
-        //       (Ensure that the the default profile picture is now displayed.)
+    const userData = {
+      username: enteredUsername,
+      updated_at: new Date(),
+      id: user.id,
+    };
 
-        // Close the popover menu.
-        handleCloseProfilePictureMenu();
-    }
+    console.log(userData);
 
-    /**
-     * Variables and functions for saving the new profile picture that is uploaded by the user. ===
-     */
-    
-    const [selectedFile, setSelectedFile] = useState();      // Eventually contains the new picture.
-    const [isFilePicked, setIsFilePicked] = useState(false); // Remove if not needed, probably.
+    const { data, error } = await supabase.from("profiles").upsert(userData, {
+      returning: "minimal", // Don't return the value after inserting
+    });
+  };
 
-    function handleChangeProfilePicture(event) {
 
-        console.log("The function handleChangeProfilePicture was called.");
+  // const resetPasswordHandler = async (event) => {
+  //   event.preventDefault();
 
-        // Set `selectedFile` to the selected file, and record that the file is picked.
-        setSelectedFile(event.target.files[0]);
-        setIsFilePicked(true);
+  //   // const oldPassword = oldPasswordInputRef.current.value
+  //   const email = emailInputRef.current.value;
+  //   const newPassword = newPasswordInputRef.current.value;
+  //   const confirmNewPassword = confirmNewPasswordInputRef.current.value;
 
-        // TODO: Save the image to the database.
-        handleSubmission();
-    }
+  //   console.log(user.id);
 
-    function handleSubmission() {
+  //   // if (user.encrypted_password !== bcrypt(oldPassword, supabase.auth.users.encrypted_password)) {
+  //   if (user.email !== email) {
+  //     console.log("Invalid Email!");
+  //   } else {
+  //     console.log("Success!");
+  //     if (newPassword === confirmNewPassword) {
+  //       const { user, error } = await supabase.auth.update({
+  //         password: { newPassword },
+  //       });
+  //     }
+  //   }
+  // };
 
-        console.log("The function handleSubmission was called.");
+  // When the profile picture is clicked, ...
+  function handleClickProfilePicture(event) {
+    setAnchorEl(event.currentTarget);
+  }
 
-        const formData = new FormData();
-        formData.append('File', selectedFile);
+  function handleCloseProfilePictureMenu() {
+    setAnchorEl(null);
+  }
 
-        fetch(
-            "https://link-to-API-goes-here",
-            {
-                method: 'POST',
-                body: formData
-            }
-        )
-            .then((response) => response.json())
-            .then((result) => {
-                console.log('Success: ", result');
-            })
-            .catch((error) => {
-                console.error('Error: ', error);
-            })
-            .then(() => {
-                // Update the user interface to display the new profile picture from the server.
-            })
-            .then(() => {
-                // Reset `selectedFile` and `isFilePicked` to `null` and `false` respectively to allow the
-                // user to change their profile picture more than one time.
-                console.log("selectedFile and isFilePicked have been reset.");
-                setSelectedFile(null);
-                setIsFilePicked(false);
-            });
+  function handleClickRemovePicture() {
+    // TODO: Delete the user's current profile picture from the database.
+    //       (Ensure that the the default profile picture is now displayed.)
 
-        
-        
-    }
+    // Close the popover menu.
+    handleCloseProfilePictureMenu();
+  }
 
-    /**
-     * Return statement ===========================================================================
-     */
+  /**
+   * Variables and functions for saving the new profile picture that is uploaded by the user. ===
+   */
 
-    return (
-        <>
-            <Typography variant="h4"> My Account </Typography>
+  const [selectedFile, setSelectedFile] = useState(); // Eventually contains the new picture.
+  const [isFilePicked, setIsFilePicked] = useState(false); // Remove if not needed, probably.
 
-            <Paper sx={{ my: 2, p: 2, width: 300 }}>
+  function handleChangeProfilePicture(event) {
+    console.log("The function handleChangeProfilePicture was called.");
 
-                <Box display="flex" justifyContent="center">
-                    <Avatar
-                        // src= TODO: Get the user's profile picture from Supabase.
-                        style={{ width: 150, height: 150 }}
-                        onClick={handleClickProfilePicture}
-                    />
+    // Set `selectedFile` to the selected file, and record that the file is picked.
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
 
-                    <Popover
-                        id={profilePictureMenuId}
-                        open={isProfilePictureMenuOpen}
-                        anchorEl={anchorEl}
-                        onClose={handleCloseProfilePictureMenu}
-                        anchorOrigin={{
-                            horizontal: 'left',
-                            vertical: 'bottom'
-                        }}
-                        sx={{
-                            flexFlow: 'column wrap'
-                        }}
-                    >
-                        <label htmlFor="contained-button-file">
-                            <Input accept="image/*" id="contained-button-file" type="file" sx={{display: 'none'}} onChange={handleChangeProfilePicture}/>
-                            <Button variant="outlined" component="span" sx={{m: 1}} startIcon={<UploadFileRoundedIcon />}>
-                                Upload Photo...
-                            </Button>
-                        </label>
+    // TODO: Save the image to the database.
+    handleSubmission();
+  }
 
-                        <Button
-                            startIcon={<DeleteRoundedIcon />}
-                            variant="outlined"
-                            sx={{m: 1}}
-                            onClick={handleCloseProfilePictureMenu}
-                        >
-                            Remove photo
-                        </Button>
-                        
-                    </Popover>
-                </Box>
+  function handleSubmission() {
+    console.log("The function handleSubmission was called.");
 
-                <TextField
-                    fullWidth
-                    defaultValue="Ben Awad"
-                    sx={{
-                        display: 'block',
-                        my: 1,
-                        input: { textAlign: "center" }
-                    }
-                    }
+    const formData = new FormData();
+    formData.append("File", selectedFile);
 
+    fetch("https://link-to-API-goes-here", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Success: ", result');
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      })
+      .then(() => {
+        // Update the user interface to display the new profile picture from the server.
+      })
+      .then(() => {
+        // Reset `selectedFile` and `isFilePicked` to `null` and `false` respectively to allow the
+        // user to change their profile picture more than one time.
+        console.log("selectedFile and isFilePicked have been reset.");
+        setSelectedFile(null);
+        setIsFilePicked(false);
+      });
+  }
+
+  /**
+   * Return statement ===========================================================================
+   */
+
+  return (
+    <>
+      <Typography variant="h4"> My Account </Typography>
+
+      <Paper sx={{ my: 2, p: 2, width: 300 }}>
+        <form onSubmit={saveUsernameHandler}>
+          <Box display="flex" justifyContent="center">
+            <Avatar
+              // src= TODO: Get the user's profile picture from Supabase.
+              style={{ width: 150, height: 150 }}
+              onClick={handleClickProfilePicture}
+            />
+
+            <Popover
+              id={profilePictureMenuId}
+              open={isProfilePictureMenuOpen}
+              anchorEl={anchorEl}
+              onClose={handleCloseProfilePictureMenu}
+              anchorOrigin={{
+                horizontal: "left",
+                vertical: "bottom",
+              }}
+              sx={{
+                flexFlow: "column wrap",
+              }}
+            >
+              <label htmlFor="contained-button-file">
+                <Input
+                  accept="image/*"
+                  id="contained-button-file"
+                  type="file"
+                  sx={{ display: "none" }}
+                  onChange={handleChangeProfilePicture}
                 />
+                <Button
+                  variant="outlined"
+                  component="span"
+                  sx={{ m: 1 }}
+                  startIcon={<UploadFileRoundedIcon />}
+                >
+                  Upload Photo...
+                </Button>
+              </label>
 
-            </Paper>
+              <Button
+                startIcon={<DeleteRoundedIcon />}
+                variant="outlined"
+                sx={{ m: 1 }}
+                onClick={handleCloseProfilePictureMenu}
+              >
+                Remove photo
+              </Button>
+            </Popover>
+          </Box>
 
-            <Paper sx={{ my: 2, p: 2, width: 300 }}>
-                <Typography variant="h5" sx={{ mb: 2 }}> Change Password </Typography>
+          <TextField
+            inputRef={usernameInputRef}
+            fullWidth
+            defaultValue="Ben Awad"
+            sx={{
+              display: "block",
+              my: 2,
+              input: { textAlign: "center" },
+            }}
+          />
+          <Button type="submit" variant="contained">
+            {" "}
+            Save changes{" "}
+          </Button>
+        </form>
+      </Paper>
 
-                <TextField
-                    label="Current Password"
-                    type="password"
-                    fullWidth
-                    sx={{ display: 'block', mb: 1 }}
-                />
+      <Paper sx={{ my: 2, p: 2, width: 300 }}>
+        {/* <form onSubmit={resetPasswordHandler}> */}
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            {" "}
+            Change Password{" "}
+          </Typography>
 
-                <TextField
-                    label="New Password"
-                    type="password"
-                    fullWidth
-                    sx={{ display: 'block', mb: 1 }}
-                />
+          <TextField
+            // inputRef={oldPasswordInputRef}
+            label="Current Password"
+            type="password"
+            fullWidth
+            sx={{ display: "block", mb: 1 }}
+          />
 
-                <TextField
-                    label="Confirm New Password"
-                    type="password"
-                    fullWidth
-                    sx={{ display: 'block', mb: 2 }}
-                />
+          <TextField
+            // inputRef={newPasswordInputRef}
+            label="New Password"
+            type="password"
+            fullWidth
+            sx={{ display: "block", mb: 1 }}
+          />
 
-                <Button variant="contained"> Change Password </Button>
+          <TextField
+            // inputRef={confirmNewPasswordInputRef}
+            label="Confirm New Password"
+            type="password"
+            fullWidth
+            sx={{ display: "block", mb: 2 }}
+          />
 
-            </Paper>
+          <Button type="submit" variant="contained">
+            {" "}
+            Reset Password{" "}
+          </Button>
+        {/* </form> */}
+      </Paper>
 
-            <Button variant="contained">Sign Out</Button>
-        </>
-    )
+      <Button variant="contained">Sign Out</Button>
+    </>
+  );
 }
